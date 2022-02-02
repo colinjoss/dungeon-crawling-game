d57bb40ada551e4c8e60ee5691a91fbe6034a2df
Colin Joss <colindjoss@gmail.com>
2022-02-02 09:27:43 -0800

    Created game loop w/ screen that can be exited

--- game.py
+++ game.py
@@ -1,4 +1,14 @@
 # Author: Colin Joss
-# Last date updated: 1/25/2022
+# Last date updated: 2/2/2022
 
 import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))    # The game window
+
+running = True
+while running:     # The game loop
+    for event in pygame.event.get():        # When input is...
+        if event.type == pygame.QUIT:       # Exit
+            running = False
+
