4dc0c16dffc6367fed764e3b0fff90cae8e4caff
Colin Joss <colindjoss@gmail.com>
2022-02-02 09:48:29 -0800

    Added window caption & display update

--- game.py
+++ game.py
@@ -6,9 +6,19 @@ pygame.init()
 
 screen = pygame.display.set_mode((640, 480))    # The game window
 
+# Title and icon
+pygame.display.set_caption('Dungeon Explorer')
+# pygame.display.set_icon(pygame.image.load("file.png"))
+
+# The game loop
 running = True
-while running:     # The game loop
-    for event in pygame.event.get():        # When input is...
-        if event.type == pygame.QUIT:       # Exit
+while running:
+
+    for event in pygame.event.get():            # When input is...
+        if event.type == pygame.QUIT:           # Exit
             running = False
+
+    pygame.display.update()
+
+
 
