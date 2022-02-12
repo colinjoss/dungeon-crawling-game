3442ac21a35adb0edaafda52bda7cf692640699e
Colin Joss <colindjoss@gmail.com>
2022-02-12 12:07:17 -0800

    Added Map and Player classes

--- game.py
+++ game.py
@@ -3,7 +3,7 @@
 
 import pygame
 from PIL import Image
-import pygame_menu
+
 
 pygame.init()
 
@@ -16,24 +16,14 @@ class Game:
     TITLE_FONT = pygame.font.Font('freesansbold.ttf', 32)
     TITLE_MENU_FONT = pygame.font.Font('freesansbold.ttf', 28)
     BAG_MENU_FONT = pygame.font.Font('freesansbold.ttf', 24)
-
-
-    PLAYER_IMG = pygame.image.load('img/player1.png')    # Player image
-    PLAYER_X = 320
-    PLAYER_Y = 240
+    CENTER = (320, 240)
 
     def __init__(self):
-        self.bg = self.get_new_bg()
-        self.map_size = self.get_map_size()
-        self.map_width = self.map_size[0] - 640
-        self.map_height = self.map_size[1] - 640
-        self.x = 320 - self.round_multiple_32(self.map_width // 2)
-        self.y = 240 - self.round_multiple_32(self.map_height // 2)
-        self.n_bound = -80
-        self.w_bound = 0
-        self.s_bound = self.n_bound - self.map_height + 32
-        self.e_bound = self.w_bound - self.map_width + 32
-
+        self.map = Map()
+        self.player = Player(
+            320 - self.round_multiple_32(self.map.width // 2),
+            240 - self.round_multiple_32(self.map.height // 2)
+        )
         self.title_menu()
 
     def title_menu(self):
@@ -102,7 +92,7 @@ class Game:
         running = True
         while running:
 
-            self.SCREEN.blit(self.bg, (self.x, self.y))
+            self.SCREEN.blit(self.map.bg, (self.player.x, self.player.y))
 
             for event in pygame.event.get():  # When input is...
 
@@ -110,23 +100,24 @@ class Game:
                 if event.type == pygame.QUIT:  # Exit
                     running = False
 
-                # Player movement
                 if event.type == pygame.KEYDOWN:
+
+                    # Player movement
                     if event.key == pygame.K_LEFT:      # Left arrow
-                        self.x += self.x_boundary(32)
+                        self.player.left(self.x_boundary(32))
                     if event.key == pygame.K_RIGHT:     # Right arrow
-                        self.x += self.x_boundary(-32)
+                        self.player.right(self.x_boundary(-32))
                     if event.key == pygame.K_UP:        # Up arrow
-                        self.y += self.y_boundary(32)
+                        self.player.up(self.y_boundary(32))
                     if event.key == pygame.K_DOWN:      # Down arrow
-                        self.y += self.y_boundary(-32)
-                    print(self.x, self.y)
+                        self.player.down(self.y_boundary(-32))
+                    print(self.player.x, self.player.y)
 
-                # Presses x (BAG MENU)
-                if event.key == pygame.K_x:
-                    self.bag_menu()
+                    # Presses x (BAG MENU)
+                    if event.key == pygame.K_x:
+                        self.bag_menu()
 
-            self.player()
+            self.SCREEN.blit(self.player.image, (self.CENTER[0], self.CENTER[1]))
 
             pygame.display.update()
 
@@ -229,16 +220,13 @@ class Game:
 
                 pygame.display.update()
 
-    def player(self):
-        self.SCREEN.blit(self.PLAYER_IMG, (self.PLAYER_X, self.PLAYER_Y))
-
     def x_boundary(self, change):
-        if self.x + change < self.e_bound or self.x + change > self.w_bound:
+        if self.player.x + change < self.map.e_bound or self.player.x + change > self.map.w_bound:
             return 0
         return change
 
     def y_boundary(self, change):
-        if self.y + change > self.n_bound or self.y + change < self.s_bound:
+        if self.player.y + change > self.map.n_bound or self.player.y + change < self.map.s_bound:
             return 0
         return change
 
@@ -246,6 +234,19 @@ class Game:
         if n % 32 != 0:
             return n - (n % 32)
         return n
+
+
+class Map:
+
+    def __init__(self):
+        self.bg = self.get_new_bg()
+        self.size = self.get_map_size()
+        self.width = self.size[0] - 640
+        self.height = self.size[1] - 640
+        self.n_bound = -80
+        self.w_bound = 0
+        self.s_bound = self.n_bound - self.height + 32
+        self.e_bound = self.w_bound - self.width + 32
 
     def get_map_size(self):
         return self.bg.get_size()
@@ -274,6 +275,31 @@ class Game:
 
         bg_border.save('img/bg_jungle.jpg')         # Save as new image (temporary)
         return pygame.image.load('img/bg_jungle.jpg')
+
+    def round_multiple_32(self, n):
+        if n % 32 != 0:
+            return n - (n % 32)
+        return n
+
+
+class Player:
+
+    def __init__(self, x, y):
+        self.image = pygame.image.load('img/player1.png')
+        self.x = x
+        self.y = y
+
+    def right(self, amount):
+        self.x += amount
+
+    def left(self, amount):
+        self.x += amount
+
+    def up(self, amount):
+        self.y += amount
+
+    def down(self, amount):
+        self.y += amount
 
 
 if __name__ == '__main__':
