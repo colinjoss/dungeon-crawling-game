b8cccf8cb01ed83206be71232f13a7e40892b999
Colin Joss <colindjoss@gmail.com>
2022-02-22 13:22:11 -0800

    Fixed bug where door appears too close to border

--- level_generation.py
+++ level_generation.py
@@ -92,7 +92,7 @@ def generate_next_maps(room):
         bridge2 = BridgeNode(room, ret_spawn)
         new_room.bridges[door_nums[0]] = bridge2
 
-        for i in range(0, rd.randint(1, 2)):
+        for i in range(0, math.floor(rd.random() * 2) + 1):
             create_door(door_nums, door_coords, rows, columns, matrix)
 
         add_bridge_placeholders(door_nums, door_coords, new_room)    # Add bridge placeholders in second room
@@ -147,7 +147,6 @@ def place_items(matrix, rows, columns, n):
             matrix[r+12][c+12] = items[math.floor(rd.random() * 6)]
 
 
-
 def get_random_dimensions():
     rows, columns = rd.randint(10, 21), rd.randint(10, 21)  # Get random dimensions
     rows, columns = rows + 24, columns + 24  # Adjust rows/columns for border
@@ -156,21 +155,32 @@ def get_random_dimensions():
 
 def create_door(door_nums, door_coords, rows, columns, matrix):
     d_num = next(door_count)
-    d_coord = (rd.randint(13, rows - 14), rd.randint(13, columns - 14))
+    # d_coord = (rd.randint(13, rows - 14), rd.randint(13, columns - 14))
+    d_coord = (math.floor(rd.random() * (rows - 24) + 12), math.floor(rd.random() * (columns - 24) + 12))
 
     # Make sure coordinate is acceptable
     i = 0
     while i < len(door_coords):
         if d_coord == door_coords[i]:
+            d_coord = get_random_coordinates(rows, columns)
             continue
-        i += 1
+        if abs(d_coord[0] - door_coords[i][0]) < 3 or abs(d_coord[1] - door_coords[i][1]) < 3:
+            d_coord = get_random_coordinates(rows, columns)
+            continue
+        if d_coord[0] > rows - 14 or d_coord[1] > columns - 14:
+            d_coord = get_random_coordinates(rows, columns)
+            continue
 
-    # or 2 > abs(d_coord[0] - door_coords[i][0]) or 2 > abs(d_coord[1] - door_coords[i][1])
+        i += 1
 
     door_nums.append(d_num)
     door_coords.append(d_coord)
 
     matrix[d_coord[0]][d_coord[1]] = d_num
+
+
+def get_random_coordinates(rows, columns):
+    return math.floor(rd.random() * (rows - 24) + 12), math.floor(rd.random() * (columns - 24) + 12)
 
 
 def create_player_spawn(rows, columns, matrix):
